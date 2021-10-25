0916f9c8b47420df3d60b232f90054814b2bff70
FeitoNoCeu <89472795+FeitoNoCeu@users.noreply.github.com>
2021-10-25 19:45:26 -0300

    Add files via upload

---
+++ Aula1_variaveis/Variaves.js
@@ -0,0 +1,5 @@
+var num =  10;
+var num2 = 300;
+var texto = "Campanha de arrecadação";
+var cliente = "Pierre Sousa";
+pontos = num + num2;
---
+++ Aula2_entrada_dados_funcao/entrada.js
@@ -0,0 +1,9 @@
+function pedirvalores(){
+    num1 = parseInt(prompt("Digite o primeiro número")); 
+    num2 = parseInt(prompt("Digite o segundo número"));
+ 
+    soma = num1 + num2
+    subtracao = num1 - num2
+    divisao = num1 / num2
+    multiplicacao = num1 * num2
+}
